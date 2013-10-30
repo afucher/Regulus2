72b54d753e159d773cd46b2da7e505e6ba4566d0
Arthur Fücher <arthurfucher@gmail.com>
2013-10-29 23:22:44 -0200

    Criado logout, e acertada pagina de HOME

---
+++ logout.php
@@ -0,0 +1,6 @@
+<?php
+
+include "./php/basic.php";
+
+logout();
+?>
--- php/basic.php
+++ php/basic.php
@@ -120,10 +120,28 @@ function login_check($mysql_con) {
  }
 }
 
+function logout(){
+  sec_session_start();
+  // Unset all session values
+  $_SESSION = array();
+  // get session parameters 
+  $params = session_get_cookie_params();
+  // Delete the actual cookie.
+  setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+  // Destroy session
+  session_destroy();
+  header('Location: ./index.html');
+}
+
 function menu(){
 echo '	<nav>';
 echo '		<ul>';
-echo '			<li><a href=".\">Home</a></li>';
+echo '			<li>';
+echo '        <a href=".\index.php">Home</a>';
+echo '        <ul>';
+echo '          <li><a href="logout.php">Logout</a></li>';
+echo '        </ul>';
+echo '      </li>';
 echo '			<li>';
 echo '				<a>Cadastro</a>';
 echo '				<ul>';
